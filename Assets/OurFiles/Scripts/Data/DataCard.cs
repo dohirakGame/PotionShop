b2faeb2a7430b9bca25d8f93a63d6619b649e2c1
Arthur <ar--r@mail.ru>
2023-11-21 23:09:31 +0300

    Фон

--- Assets/OurFiles/Scripts/Data/DataCard.cs
+++ Assets/OurFiles/Scripts/Data/DataCard.cs
@@ -6,20 +6,18 @@ namespace Data
 {
     public class DataCard : MonoBehaviour
     {
-        [Header("Изображения под цвета")]
+        [Header("Фон")]
         public List<Sprite> redCardImage;
-        public List<Sprite> redItemImage;
-
         public List<Sprite> greenCardImage;
-		public List<Sprite> greenItemImage;
-
 		public List<Sprite> blueCardImage;
-		public List<Sprite> blueItemImage;
-
 		public List<Sprite> yellowCardImage;
-		public List<Sprite> yellowItemImage;
-
 		public List<Sprite> blackCardImage;
+
+        [Header("Предметы на картах")]
+        public List<Sprite> redItemImage;
+		public List<Sprite> greenItemImage;
+		public List<Sprite> blueItemImage;
+		public List<Sprite> yellowItemImage;
 		public List<Sprite> blackItemImage;
 
 		[Header("Изображения под цвет карты")]
