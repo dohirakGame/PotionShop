71683dbf16d65a8de42094461118f245d5041eb7
Arthur <ar--r@mail.ru>
2024-10-20 03:08:48 +0300

    2

--- Assets/OurFiles/Scripts/Game Logic/Card/Card.cs
+++ Assets/OurFiles/Scripts/Game Logic/Card/Card.cs
@@ -15,14 +15,14 @@ namespace Game_Logic.CardLogic
 
         [SerializeField] private Image _image;
         [SerializeField] private int _point;
-        private CardState _state;
+        //private CardState _state;
         //public CardColor _color;
         [SerializeField] private CardColor _color;
         [SerializeField] private CardBonusType _bonus;
         [SerializeField] private BonusColor _bonusColor;
         [SerializeField] private SecondBonusColor _secondBonusColor;
 
-        public void SetState(CardState state) => _state = state;
+        //public void SetState(CardState state) => _state = state;
         public void SetColor(CardColor color) => _color = color;
         public void SetBonus(CardBonusType bonus) => _bonus = bonus;
         public void SetBonusColor(BonusColor bonusColor) => _bonusColor = bonusColor;
